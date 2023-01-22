dfce8b5e9730c40f3f6e536a65570cf23120b49c
chriskimty <christaeyoung.kim@gmail.com>
2023-01-22 17:27:24 -0500

    added basic criteria

---
+++ index.js
@@ -0,0 +1,10 @@
+// 8 x 8 grid
+// 2 players
+// pawns move 1 space forward
+// at initial position, pawn can move 2 spaces forward
+// 8 pawns for each player
+
+// function that takes in a chessboard, list of movements, and output what the chessboard looks like at the end
+// pawns can overlap for the first iteration
+// list should be the chronological order moves 
+
